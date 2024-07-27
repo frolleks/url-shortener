f84d04eea3e34d740ddc05d3101197b6c7f81679
frolleks <frolleks@hotmail.com>
2024-07-27 15:13:45 +0700

    feat: reload after every action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,13 +3,18 @@
 import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { urls } from "./schema";
+import { revalidatePath } from "next/cache";
 
 export async function deleteUrl(id: string, formData: FormData) {
   await db.delete(urls).where(eq(urls.id, id));
+
+  revalidatePath("/");
 }
 
 export async function shrinkUrl(formData: FormData) {
   await db.insert(urls).values({
     destinationUrl: formData.get("url") as string,
   });
+
+  revalidatePath("/");
 }
