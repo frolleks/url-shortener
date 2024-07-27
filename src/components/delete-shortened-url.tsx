c040fd3b1c4891b1e152567c9633b5c5f407f0c3
frolleks <frolleks@hotmail.com>
2024-07-27 15:13:20 +0700

    refactor: add proper styles and props

--- src/components/delete-shortened-url.tsx
+++ src/components/delete-shortened-url.tsx
@@ -2,13 +2,17 @@
 
 import { deleteUrl } from "@/lib/actions";
 
-export function DeleteShortenedUrl({ url }: { url: { id: string } }) {
+export function DeleteShortenedUrl({
+  url,
+}: {
+  url: { id: string; destinationUrl: string };
+}) {
   const deleteUrlWithId = deleteUrl.bind(null, url.id);
 
   return (
-    <form action={deleteUrlWithId}>
-      <button type="submit">
-        <p className="text-red-700">Delete</p>
+    <form action={deleteUrlWithId} className="w-full h-full">
+      <button type="submit" className="flex w-full h-full">
+        <p className="text-red-700 justify-start">Delete</p>
       </button>
     </form>
   );
