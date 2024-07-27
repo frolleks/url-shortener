d223f055541a2c6d332491b6361701f58a29137c
frolleks <frolleks@hotmail.com>
2024-07-27 14:25:05 +0700

    fix: remove http and https check because it doesnt work

--- src/components/shortener-form.tsx
+++ src/components/shortener-form.tsx
@@ -3,18 +3,9 @@ import { Input } from "@/components/ui/input";
 import { db } from "@/lib/db";
 import { urls } from "@/lib/schema";
 
-export function ShortenerForm() {
+export async function ShortenerForm() {
   async function shrinkUrl(formData: FormData) {
     "use server";
-
-    if (
-      !formData
-        .get("url")
-        ?.toString()
-        .startsWith("http:" || "https:")
-    ) {
-      return "URL must start with https: or http:";
-    }
 
     await db.insert(urls).values({
       destinationUrl: formData.get("url") as string,
@@ -23,7 +14,7 @@ export function ShortenerForm() {
 
   return (
     <form action={shrinkUrl}>
-      <div className="flex">
+      <div className="flex gap-1.5">
         <Input placeholder="Enter a URL..." name="url" type="url" />
         <Button type="submit">Shrink</Button>
       </div>
