74a0b8de18e77db01eb23b602843fa109b1b302b
frolleks <frolleks@hotmail.com>
2024-07-27 14:25:17 +0700

    feat: create a table

---
+++ src/app/columns.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+export type URL = {
+  id: string;
+  destinationUrl: string;
+};
+
+export const columns: ColumnDef<URL>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "destinationUrl",
+    header: "Destination",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const url = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(
+                  `${window.location.href}${url.id}`
+                )
+              }
+            >
+              Copy shrunk URL
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <p className="text-red-700">Delete</p>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 import { ShortenerForm } from "@/components/shortener-form";
+import { db } from "@/lib/db";
+import { urls } from "@/lib/schema";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
 
-export default function Home() {
-  return <ShortenerForm />;
+async function getShortenedUrls() {
+  return await db.select().from(urls);
+}
+
+export default async function Home() {
+  const shortenedUrls = await getShortenedUrls();
+
+  return (
+    <div className="container mx-auto py-8">
+      <ShortenerForm />
+      <div className="mt-1.5">
+        <DataTable columns={columns} data={shortenedUrls} />
+      </div>
+    </div>
+  );
 }
